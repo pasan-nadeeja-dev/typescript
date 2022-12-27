965111eb8342b94df9345877152307d54bb3bf03
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2022-12-27 15:16:32 +0530

    add array types

--- function.ts
+++ function.ts
@@ -1,4 +1,4 @@
-// types in function paramas
+// 01. types in function paramas
 
 function userLogin(email: string, password: string, isPaid:boolean){
     return "Success";
@@ -6,10 +6,49 @@ function userLogin(email: string, password: string, isPaid:boolean){
 
 userLogin("p@gmail.com","1qaz@WSX",false);
 
-// return void function in typescript
+// 02. return void function in typescript
 
 const getValue = (age: number):void => {}
 
-// return string/number function in typescript
+// 03. return string/number function in typescript
 
 const getName = (info: string) :string => "first-name"
+
+// 04. return union types
+
+function getStatus(age: number): boolean | string | never {
+    if(age > 5) return false;
+    return "success"
+}
+
+// 05. option type is not required
+// readonly types cannot be reassigned (primitive properties)
+
+type user = {
+    readonly email: string,
+    name: string
+    phone?: number
+}
+
+let student: user = {
+    email: "p@gmail.com",
+    name: "ps"
+}
+
+// student.email = "t@gmail.com" cannot reassign beacuse of readonly property
+
+// 06. intersection type
+// able to combine types
+
+type email = {
+    address: string
+}
+
+type boy = email & {
+    firstName: string
+}
+
+let boyObj: boy = {
+    address: "sdf",
+    firstName: "er"
+}
